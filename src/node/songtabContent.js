c2e56b4b73c73c40910f2e5f892da38c25dd17ec
Hrealm <952172047@qq.com>
2019-08-27 20:01:29 +0800

    fix: fix songImg

--- src/node/songtabContent.js
+++ src/node/songtabContent.js
@@ -20,7 +20,7 @@ let songtabContent = {
                 title: '人造卫星情人',
                 artist: '摩登兄弟',
                 src: 'http://183.60.131.241/amobile.music.tc.qq.com/C400004V4w6701ZyIc.m4a?guid=3518953300&vkey=5D55B6C984EF23F69A713C51A85A1F71A4099B59C51C1E376B3AE28AB93BF96EC1F5787CF815CFA4CC08154353DE85D7A61B790052A5A7D9&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Chinese/2.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -32,7 +32,7 @@ let songtabContent = {
                 title: '假如我很快乐',
                 artist: '麦小兜',
                 src: 'http://113.96.101.19/amobile.music.tc.qq.com/C400002MLT860TOLGs.m4a?guid=3518953300&vkey=5FE6A77A470A0380F1084CF7FD70577304D0133F6FDD5933F899F893CC2F4BE7924EC4E6CDF21D02D10660AE68463B0F7E7E5596EE6D3BFE&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Chinese/3.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -44,7 +44,7 @@ let songtabContent = {
                 title: '数羊',
                 artist: '王蓉',
                 src: 'http://183.60.23.27/amobile.music.tc.qq.com/C400002ZeqAj2x1ZTD.m4a?guid=3518953300&vkey=B796FD3183E30507659DE7F9954344EF70EA37B46C98F76AB8966C1610E9DF1CC971D6151953F876363C49CFEB87440D817DDB8384582126&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Chinese/4.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -56,7 +56,7 @@ let songtabContent = {
                 title: '好好说【花椒之味电影主题曲】',
                 artist: '郑秀文',
                 src: 'http://183.60.23.13/amobile.music.tc.qq.com/C400003An3K12sTjqM.m4a?guid=3518953300&vkey=5F0E694E6BF659887E57DCA2196EF1167BEAD82C19A7799A6EC670FF77C5D913394A70EE0E4CDCC9474FC6DDA129E852DAAC5085E49DF8A9&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Chinese/5.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -68,7 +68,7 @@ let songtabContent = {
                 title: '莫问归期',
                 artist: '蒋雪儿',
                 src: 'http://183.60.131.181/amobile.music.tc.qq.com/C400003gUhuW1tYujy.m4a?guid=3518953300&vkey=C08514D05CDFF57BB073CA7B2C2CEE218611981DB814A14E6DF0EC6665D886BAD82748FC219953FB56689BFE6F6CB2F5E1DCF40056651F40&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Chinese/6.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -80,7 +80,7 @@ let songtabContent = {
                 title: '言不由衷',
                 artist: '小倩',
                 src: 'http://183.60.131.109/amobile.music.tc.qq.com/C4000028XjNv0lNfGG.m4a?guid=3518953300&vkey=8D88452F30C349AED45310A49F90401FC64E6B2951ED010A75B00A2F32CCFDF70497035B735E4CF429F8279A54133FDA46B1A02A676193CE&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Chinese/7.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -92,7 +92,7 @@ let songtabContent = {
                 title: '只想跟你在一起【国民老公2》影视剧片尾曲】',
                 artist: '熊梓淇、赖雨濛',
                 src: 'http://183.60.131.241/amobile.music.tc.qq.com/C400000bfijg39ywUF.m4a?guid=3518953300&vkey=0986B5215C662CA5CAFB164CD98F2F3438B8A152EAC58B4AC7740CFCAF62D5D63BA7B1AF32733580C21619B09986E9EA2C079C71917F7B61&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Chinese/8.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         }
@@ -106,7 +106,7 @@ let songtabContent = {
                 title: 'Lover',
                 artist: 'Selina Mour',
                 src: 'https://music.163.com/song/media/outer/url?id=554191364.mp3',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/USEuro/1.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -118,7 +118,7 @@ let songtabContent = {
                 title: 'Home',
                 artist: 'Martin Garrix、Bonn',
                 src: 'http://113.96.101.17/amobile.music.tc.qq.com/C4000007tRFz2gnI8k.m4a?guid=3518953300&vkey=9E2E486D4E6D9FEFE4C0C47215E61360713E85401B21C3941247555086FDDE6A7EB5A81823E323E7EA96FAF1B06CA7F740F5EF41900F89E0&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/USEuro/2.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -130,7 +130,7 @@ let songtabContent = {
                 title: 'Won t Be Late',
                 artist: 'Swae Lee、Drake',
                 src: 'http://113.105.155.30/amobile.music.tc.qq.com/C400002eP7vg0vXpM3.m4a?guid=3518953300&vkey=3539D9343D313C5FEA399E26A04DF6DD147CD3AC349ECB0EFDF03903C16CBCBC1C26CAFE763862236F8AC9EB27E7EF4138432192919ECA57&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/USEuro/3.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -142,7 +142,7 @@ let songtabContent = {
                 title: 'Torn',
                 artist: 'Ava Max',
                 src: 'http://ws.stream.qqmusic.qq.com/C400004CoHVI4Tnf4l.m4a?guid=3518953300&vkey=939B3340972E3643E0A1C99D85C09F9AD9E2B88EE69234898D361B42E4C8F2A51632890387F647F14038B01CEBE0F5BAF97BC9E030256890&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/USEuro/4.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -154,7 +154,7 @@ let songtabContent = {
                 title: '#PROUDCATOWNERREMIX',
                 artist: 'XXXTENTACION、Rico Nasty',
                 src: 'http://183.60.23.19/amobile.music.tc.qq.com/C400003PFXpJ3rMyS5.m4a?guid=3518953300&vkey=256F28A02CEB8004ED39A4889AE43871EEA0CB26CA266694835D436745BE417DBB9A2FA7D7093FC06D218B999660C3B260067D27923402CB&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/USEuro/5.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -166,7 +166,7 @@ let songtabContent = {
                 title: 'I Don t Think I Can Do This Again',
                 artist: 'Mura Masa、Clairo',
                 src: 'http://113.96.101.23/amobile.music.tc.qq.com/C400002HJvut1JgSWr.m4a?guid=3518953300&vkey=42C0782C9EB861809E8C6BACBDC030CB8FB792DC7EEA4A7213D2491B42AA6C213B4C37B560C368E737A85F989C15D422C4066765EDBDBDC6&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/USEuro/6.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -178,7 +178,7 @@ let songtabContent = {
                 title: 'NO HALO',
                 artist: 'BROCKHAMPTON',
                 src: 'http://ws.stream.qqmusic.qq.com/C400002t5XJo2uDlwG.m4a?guid=3518953300&vkey=39633A453BEE71BE45B89C59C68656F97FDF57C529EB767672C8D0CF968ECB648A8C683CA001F44DA384AB623A57B16D23D7800CE8D72D37&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/USEuro/7.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -190,7 +190,7 @@ let songtabContent = {
                 title: 'Desert Moon (From  Aladdin )',
                 artist: 'Mena Massoud、Naomi Scott',
                 src: 'http://121.9.201.18/amobile.music.tc.qq.com/C400000mctle0TFRdI.m4a?guid=3518953300&vkey=E00EDE6D0FF7C22A039E4F5C53842E5B0A4F71779F8E38532093B8C8A40348FFA31D8BDE472EBCC3632C6071F7302BB0C35A2454706F9E7C&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/USEuro/8.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         }
@@ -204,7 +204,7 @@ let songtabContent = {
                 title: '음파음파 (Umpah Umpah)',
                 artist: '顾三十',
                 src: 'https://music.163.com/song/media/outer/url?id=1385748156.mp3',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Korea/1.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -216,7 +216,7 @@ let songtabContent = {
                 title: '그 끝에 그대 (at the end)【德鲁纳酒店 OST】',
                 artist: '倩倩1204',
                 src: 'https://music.163.com/song/media/outer/url?id=1386556138.mp3',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Korea/2.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -228,7 +228,7 @@ let songtabContent = {
                 title: '어떤 별보다 (See the star)【德鲁纳酒店 OST】',
                 artist: 'Red Velvet',
                 src: 'https://music.163.com/song/media/outer/url?id=1383775696.mp3',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Korea/3.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -240,7 +240,7 @@ let songtabContent = {
                 title: '기억하나요 내 모든 날과 그때를 (Remember me)【德鲁纳酒店 OST】',
                 artist: '倩倩1204',
                 src: 'https://music.163.com/song/media/outer/url?id=1385959151.mp3',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Korea/4.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -252,7 +252,7 @@ let songtabContent = {
                 title: 'LEOPARD (Prod. Cosmic Boy)',
                 artist: '라비(RAVI)、颂乐',
                 src: 'http://113.105.155.15/amobile.music.tc.qq.com/C4000015vd4X2goQYz.m4a?guid=3518953300&vkey=B5E7409F2C643353A91355209E7813EA0B655215F04C5D9A01C1B3CD0FD43B38FE1CEBC224FE7B7C479C062B10AEA4AEDF461CBB4456C747&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Korea/5.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -264,7 +264,7 @@ let songtabContent = {
                 title: 'CALL ANYTIME',
                 artist: 'JINU(金秦禹)、MINO(宋旻浩)',
                 src: 'http://ws.stream.qqmusic.qq.com/C400000hhr7j3TYu9C.m4a?guid=3518953300&vkey=40D262C533088218AC22A027F5F4A1FC6053E5B733610A5BB822464C51726518637A6164248DF518ED9C4AE2D4CC1FDA8D861810E2AE17EB&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Korea/6.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -276,7 +276,7 @@ let songtabContent = {
                 title: 'My Dream【新入史官具海玲 OST】',
                 artist: '尹美莱',
                 src: 'http://14.29.101.39/amobile.music.tc.qq.com/C400000Qk88f1xlXiR.m4a?guid=3518953300&vkey=241A91ADD01E4E9777F3889C7E72CAEE3C648FCE2D7F6F5B91B6376B92E39FE377FE3D4009709DD4DB4532F0744E700140FF1178B88F9500&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Korea/7.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -288,7 +288,7 @@ let songtabContent = {
                 title: '36.5',
                 artist: 'SHAUN',
                 src: 'http://113.105.141.22/amobile.music.tc.qq.com/C4000044Yj0x0f9hY7.m4a?guid=3518953300&vkey=1C24268A68CD56D696A15DD4E14172E49FC3D524365B6F1E6DE9AC07FD1C355F9C21E315B1A4F21289934AD59DC17E0353203A94997BC26F&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Korea/8.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         }
@@ -302,7 +302,7 @@ let songtabContent = {
                 title: 'DREAMERS',
                 artist: 'GENERATIONS from EXILE TRIBE',
                 src: 'http://113.105.166.107/amobile.music.tc.qq.com/C400002ohXT40ti1gQ.m4a?guid=3518953300&vkey=9C7F965E7B573DD7A24E4E1CDDA69AEF83A49B63DE0530CE0BE2CEA4145E6279A0824D6466501C038E8CA8665303A70FEB130F298F9EDB25&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Japan/1.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -314,7 +314,7 @@ let songtabContent = {
                 title: 'Ghosts of the Sea【海兽之子主题曲】',
                 artist: '米津玄師',
                 src: 'http://113.105.141.24/amobile.music.tc.qq.com/C400002XKOFt1ZayiY.m4a?guid=3518953300&vkey=1CE930AA4529301C5141C5197C13C47F1E4334CC4239FB98647D98DE0C91FD33FD62C7B6326BF984635DAAE8F679E8E48A9886616A8B6DDF&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Japan/2.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -326,7 +326,7 @@ let songtabContent = {
                 title: 'Lights',
                 artist: 'BTS（防弹少年团）',
                 src: 'http://113.105.141.23/amobile.music.tc.qq.com/C400000oyUh538A7sT.m4a?guid=3518953300&vkey=92EBCCCC0766EC3DFF7B379953F383AA6C3EEA42F006290F3817B1E9FE19D3B3FA915A4975A62314AD98B2F90FC136860F244891BF0D880E&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Japan/3.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -338,7 +338,7 @@ let songtabContent = {
                 title: 'Veil【炎炎消防队片尾曲】',
                 artist: '須田景凪',
                 src: 'http://113.105.141.23/amobile.music.tc.qq.com/C400001KpIKP3OkKiY.m4a?guid=3518953300&vkey=81A0AEF14934196C24FD425D23BAEB44137D301D1E76D910901037276EF8037C2BB45D27B9A1925BBEADCC89F37AA21B24102D031BA1003C&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Japan/4.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -350,7 +350,7 @@ let songtabContent = {
                 title: 'Torches【海盗战记片尾曲】',
                 artist: 'Aimer',
                 src: 'http://isure.stream.qqmusic.qq.com/C400003jtqQ109Mqno.m4a?guid=3518953300&vkey=8E0CB3FD07174F21BA0BB065E463AC72C7BA71811523E5728CAAD2D66A536CAEA1F058A1253B7CD0BDB6073A6E1758022F1A2A80425ECDAA&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Japan/5.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -362,7 +362,7 @@ let songtabContent = {
                 title: 'Happy Happy',
                 artist: 'TWICE',
                 src: 'http://183.60.23.21/amobile.music.tc.qq.com/C400002tDGHT3DI3B2.m4a?guid=3518953300&vkey=4FD67F2D68FFDC383744418883863873D45AAD1DC23B62B7D88F35B317441643C490ADB56503FBE363B81B7F26C7E5CD4AFF3C275030EE3C&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Japan/6.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -374,7 +374,7 @@ let songtabContent = {
                 title: 'Carry on',
                 artist: 'Kygo/Rita Ora',
                 src: 'http://14.29.101.37/amobile.music.tc.qq.com/C400001ZpqAh43h1lI.m4a?guid=3518953300&vkey=9BDC1A02D92AF2BDE14AB504833844147FDCA09F4D3BF807FEBB3961CB30F4C65D173176978D1A0C7A73BDBA0388E40AF910B581F945178D&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Japan/7.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         },
@@ -386,7 +386,7 @@ let songtabContent = {
                 title: '備忘録',
                 artist: 'GOT7',
                 src: 'http://113.113.69.169/amobile.music.tc.qq.com/C400001iNs8k1eMJRo.m4a?guid=3518953300&vkey=6BFF3015EAC0C4E795A4E7BFBBAC350979D698F583D4976342E05997D9A133735ED5C17578411AC1155CA3FD82170C8EF4920EA46BF991D0&uin=3117&fromtag=66',
-                pic: '../static/img/songList/1.jpg',
+                pic: '../static/img/songtabContent/Japan/8.jpg',
                 lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
             }
         }

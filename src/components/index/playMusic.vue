cddb64ad846e4dd234fe3e1ff8d6b32bb7be9cc5
Hrealm <952172047@qq.com>
2019-08-27 20:56:30 +0800

    feat: update player

--- src/components/index/playMusic.vue
+++ src/components/index/playMusic.vue
@@ -1,11 +1,21 @@
 <template>
     <div class="wrap">
+        <!-- playMusic -->
         <div class="playMusic">
             <aplayer autoplay :music="songList" showLrc :list="list" repeat="repeat-all" v-if="player"></aplayer>
         </div>
-        <div class="recommend">
-            
+        <!-- secoundContent -->
+        <div class="secoundContent clearFix">
+            <!-- 相关推荐 -->
+            <div class="recommend fl">
+                <div class="itemTitle clearFix">
+                    <h3>相关<b>推荐</b></h3>
+                    <a href="javascript:;">更多</a>
+                </div>
+            </div>
+            <!--  -->
         </div>
+
     </div>
 
 </template>
@@ -55,6 +65,38 @@ export default {
             margin: 0 auto;
             padding-top: 30px;
             border-top: 1px solid #ddd;
+        }
+        .secoundContent{
+            width: 1200px;
+            margin: 0 auto;
+            margin-top: 40px;
+            .recommend{
+                width: 820px;
+                .itemTitle {
+					width: 100%;
+					height: 30px;
+					line-height: 30px;
+					padding-bottom: 30px;
+					h3 {
+						float: left;
+						display: block;
+						width: 120px;
+						height: 30px;
+						color: #31c27c;
+						letter-spacing: 5px;
+						font-weight: 400;
+						b {
+							font-weight: 400;
+							color: #555;
+						}
+					}
+					a {
+						float: right;
+						font-size: 13px;
+						color: #999;
+					}
+				}
+            }
         }
     }
 

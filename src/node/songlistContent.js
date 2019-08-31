c67754bdbe5fad7bb5512db20d7a627b7f1720c6
Hrealm <952172047@qq.com>
2019-09-01 00:49:41 +0800

    feat: update nodeJson

--- src/node/songlistContent.js
+++ src/node/songlistContent.js
@@ -2,62 +2,92 @@ let songlist = {
     SoaringList: [
         {
             id: 1,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            name: 'K-391、Alan Walker、Tungevaag、Mangoo - Play',
+            time: '2:48',
             musicInfo: {}
         },
         {
             id: 2,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            name: '大欢 - 谁才是真心爱我的人',
+            time: '4:37',
             musicInfo: {}
         },
         {
             id: 3,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            name: 'HITA - 羡云',
+            time: '3:06',
             musicInfo: {}
         },
         {
             id: 4,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            name: 'WALK THE MOON、BRÅVES - Lose You Again',
+            time: '3:36',
             musicInfo: {}
         },
         {
             id: 5,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            name: 'G.E.M.邓紫棋、Obi、刘炫廷、Capper、福克斯 - We Are Young (L...',
+            time: '3:52',
             musicInfo: {}
         },
         {
-            id: 1,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            id: 6,
+            name: '杨紫 - 有风的夜晚',
+            time: '3:00',
             musicInfo: {}
         },
         {
-            id: 2,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            id: 7,
+            name: '兰四夕 - 向江南折过花',
+            time: '3:01',
             musicInfo: {}
         },
         {
-            id: 3,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            id: 8,
+            name: 'BLACKPINK - SOLO (Live)',
+            time: '3:01',
             musicInfo: {}
         },
         {
-            id: 4,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            id: 9,
+            name: '吴青峰 - 太空人',
+            time: '5:18',
             musicInfo: {}
         },
         {
-            id: 5,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            id: 10,
+            name: 'Billie Eilish - bury a friend',
+            time: '3:13',
+            musicInfo: {}
+        },
+        {
+            id: 11,
+            name: '徐啸枭 - 忘了吧',
+            time: '4:33',
+            musicInfo: {}
+        },
+        {
+            id: 12,
+            name: '陈秋霞 - I Will Follow Him',
+            time: '2:31',
+            musicInfo: {}
+        },
+        {
+            id: 13,
+            name: '陈其楠 - 拿走了什么 (Live)',
+            time: '4:28',
+            musicInfo: {}
+        },
+        {
+            id: 14,
+            name: '雨中百合 - 分手',
+            time: '4:15',
+            musicInfo: {}
+        },
+        {
+            id: 15,
+            name: '鹿晗 - 夜的尽头',
+            time: '3:11',
             musicInfo: {}
         }
     ],
@@ -65,61 +95,91 @@ let songlist = {
         {
             id: 1,
             name: '音阙诗听、赵方婧 - 芒种',
-            time: '4:49',
+            time: '3:36',
             musicInfo: {}
         },
         {
             id: 2,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            name: '阿悠悠 - 念旧',
+            time: '3:49',
             musicInfo: {}
         },
         {
             id: 3,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            name: 'G.E.M.邓紫棋 - 画 (Live Piano Session II)',
+            time: '2:49',
             musicInfo: {}
         },
         {
             id: 4,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            name: '童珺 - 天下有情人 (国语版)',
+            time: '6:44',
             musicInfo: {}
         },
         {
             id: 5,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            name: '童珺 - 天下有情人 (国语版)',
+            time: '2:11',
             musicInfo: {}
         },
         {
-            id: 1,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            id: 6,
+            name: 'Cedric Gervais、Digital Farm Animals、Dallas Austin - Touch...',
+            time: '2:36',
             musicInfo: {}
         },
         {
-            id: 2,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            id: 7,
+            name: '程jiajia - 山楂树の恋',
+            time: '3:04',
             musicInfo: {}
         },
         {
-            id: 3,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            id: 8,
+            name: '李宗盛 - 不必在乎我是谁 (Live)',
+            time: '4:16',
             musicInfo: {}
         },
         {
-            id: 4,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            id: 9,
+            name: 'Shawn Mendes、Camila Cabello - Señorita',
+            time: '3:11',
             musicInfo: {}
         },
         {
-            id: 5,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            id: 10,
+            name: '魏新雨 - 余情未了',
+            time: '3:36',
+            musicInfo: {}
+        },
+        {
+            id: 11,
+            name: '小阿枫 - 最远的你是我最近的爱',
+            time: '2:16',
+            musicInfo: {}
+        },
+        {
+            id: 12,
+            name: '浙音4811 - 拜拜',
+            time: '3:21',
+            musicInfo: {}
+        },
+        {
+            id: 13,
+            name: 'K-391、Alan Walker、Tungevaag、Mangoo - Play',
+            time: '2:48',
+            musicInfo: {}
+        },
+        {
+            id: 14,
+            name: '王力宏、谭维维 - 缘分一道桥',
+            time: '4:06',
+            musicInfo: {}
+        },
+        {
+            id: 15,
+            name: '潘玮柏、G.E.M.邓紫棋、艾热 - 攀登 (Live)',
+            time: '4:11',
             musicInfo: {}
         }
     ],
@@ -127,61 +187,91 @@ let songlist = {
         {
             id: 1,
             name: '虎二 - 你一定要幸福',
-            time: '4:49',
+            time: '4:19',
             musicInfo: {}
         },
         {
             id: 2,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            name: '杭娇 - 听心',
+            time: '3:56',
             musicInfo: {}
         },
         {
             id: 3,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            name: '音阙诗听、赵方婧 - 芒种',
+            time: '3:36',
             musicInfo: {}
         },
         {
             id: 4,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            name: '冷漠、杨小曼 - 心锁',
+            time: '4:16',
             musicInfo: {}
         },
         {
             id: 5,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            name: '孙艺琪 - 最远的你是我最近的爱',
+            time: '4:17',
             musicInfo: {}
         },
         {
-            id: 1,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            id: 6,
+            name: '魏新雨 - 余情未了',
+            time: '3:36',
             musicInfo: {}
         },
         {
-            id: 2,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            id: 7,
+            name: '任妙音 - 天在下雨我在想你',
+            time: '4:26',
             musicInfo: {}
         },
         {
-            id: 3,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            id: 8,
+            name: '冷漠、杨小曼 - 爱如星火',
+            time: '4:20',
             musicInfo: {}
         },
         {
-            id: 4,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            id: 9,
+            name: '冷漠、杨小曼 - 爱如星火',
+            time: '4:20',
             musicInfo: {}
         },
         {
-            id: 5,
-            name: '刘汉杰 - 其实你不爱我',
-            time: '4:49',
+            id: 10,
+            name: '兔子牙 - 小白兔遇上卡布奇诺',
+            time: '2:43',
+            musicInfo: {}
+        },
+        {
+            id: 11,
+            name: '大欢 - 多年以后',
+            time: '4:03',
+            musicInfo: {}
+        },
+        {
+            id: 12,
+            name: 'Ice Paper - 心如止水',
+            time: '3:05',
+            musicInfo: {}
+        },
+        {
+            id: 13,
+            name: '顾小洁 - 遇到 (女生版)',
+            time: '3:02',
+            musicInfo: {}
+        },
+        {
+            id: 14,
+            name: '王琪 - 万爱千恩',
+            time: '5:22',
+            musicInfo: {}
+        },
+        {
+            id: 15,
+            name: '花姐 - 狂浪',
+            time: '3:01',
             musicInfo: {}
         }
     ],

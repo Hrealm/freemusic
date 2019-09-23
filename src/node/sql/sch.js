8588e10027c7ad7a801baf67100a90bb874734b4
Hrealm <952172047@qq.com>
2019-09-23 23:24:14 +0800

    feat: updated sqlschema

--- src/node/sql/sch.js
+++ src/node/sql/sch.js
@@ -5,3 +5,13 @@
 const mongoose = require('mongoose');
 
  //用户详情
+const userSchema = new mongoose.Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: true}
+});
+
+//创建表
+const user = mongoose.model('user', userSchema);
+
+//导出用户表，方便其他模块进行增删改查操作
+module.exports = {user};

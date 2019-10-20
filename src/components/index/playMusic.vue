27e6cccacd08e533ccbc8359eeee20ffbe37e6cb
Hrealm <952172047@qq.com>
2019-10-20 19:53:23 +0800

    fix: livere comment

--- src/components/index/playMusic.vue
+++ src/components/index/playMusic.vue
@@ -42,22 +42,7 @@
             <!-- 评论系统 -->
             <div class="comment fl">
                 <!-- 来必力City版安装代码 -->
-                <div id="lv-container" data-id="city" data-uid="MTAyMC80NzE3My8yMzY3Mw==">
-                    <!-- <script type="text/javascript">
-                        (function(d, s) {
-                            var j, e = d.getElementsByTagName(s)[0];
-
-                            if (typeof LivereTower === 'function') { return; }
-
-                            j = d.createElement(s);
-                            j.src = 'https://cdn-city.livere.com/js/embed.dist.js';
-                            j.async = true;
-
-                            e.parentNode.insertBefore(j, e);
-                        })(document, 'script');
-                    </script>
-                <noscript> 为正常使用来必力评论功能请激活JavaScript</noscript> -->
-                </div>
+                <div id="lv-container" data-id="city" data-uid="MTAyMC80NzE3My8yMzY3Mw=="></div>
                 <!-- City版安装代码已完成 -->
 
                 <!-- <div class="comment_title clearFix">
